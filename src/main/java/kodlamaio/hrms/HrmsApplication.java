347c91bb025aa6716c2f58e7ba967c61a0755118
Mahmud <mahmudfrzl@mail.ru>
2021-05-24 23:30:10 +0400

    Swagger Support

--- src/main/java/kodlamaio/hrms/HrmsApplication.java
+++ src/main/java/kodlamaio/hrms/HrmsApplication.java
@@ -2,16 +2,32 @@ package kodlamaio.hrms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-import kodlamaio.hrms.api.controllers.JobPositionsController;
-import kodlamaio.hrms.business.concretes.JobPositionManager;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class HrmsApplication {
 
 	public static void main(String[] args) {
 		
 		SpringApplication.run(HrmsApplication.class, args);
 	}
+	
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+                                 
+          .build();                                           
+    }
 
 }
--- src/main/java/kodlamaio/hrms/api/controllers/JobPositionsController.java
+++ src/main/java/kodlamaio/hrms/api/controllers/JobPositionsController.java
@@ -24,4 +24,5 @@ public class JobPositionsController {
 	public List<JobPosition> getAll(){
 		return jobPositionService.getAll();
 	}
+
 }
