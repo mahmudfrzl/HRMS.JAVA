daeec114e1d2ec3716a31d7f5858352a144851a8
Mahmud <mahmudfrzl@mail.ru>
2021-05-25 20:07:50 +0400

    Position Finished

--- src/main/java/kodlamaio/hrms/business/abstracts/JobPositionService.java
+++ src/main/java/kodlamaio/hrms/business/abstracts/JobPositionService.java
@@ -2,10 +2,11 @@ package kodlamaio.hrms.business.abstracts;
 
 import java.util.List;
 
+import kodlamaio.hrms.core.utilities.results.DataResult;
+
 import kodlamaio.hrms.entities.concretes.JobPosition;
 
-public interface JobPositionService {
+public interface JobPositionService {	
+	DataResult<List<JobPosition>>  getAll();
 
-	
-	List<JobPosition> getAll();
 }
--- src/main/java/kodlamaio/hrms/business/concretes/JobPositionManager.java
+++ src/main/java/kodlamaio/hrms/business/concretes/JobPositionManager.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.JobPositionService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
 import kodlamaio.hrms.dataAccess.abstracts.JobPositionDao;
 import kodlamaio.hrms.entities.concretes.JobPosition;
 @Service
@@ -22,9 +25,11 @@ public class JobPositionManager implements JobPositionService{
 		this.jobPositionDao = jobPositionDao;
 	}
 	@Override
-	public List<JobPosition> getAll() {
-		return jobPositionDao.findAll();
+	public DataResult<List<JobPosition> > getAll() {
+		return new SuccessDataResult<List<JobPosition>>(jobPositionDao.findAll(), "Veriler Listelendi");
+				
 	}
+
 
 
 
